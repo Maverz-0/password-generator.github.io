04e1fd479ec8c93283a62e1df2896658efd36234
Raul Matei <maverz@Rauls-MacBook-Air.local>
2025-03-11 20:00:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const passwordField = document.getElementById('password');
+    const generateBtn = document.getElementById('generate-btn');
+    const copyBtn = document.getElementById('copy-btn');
+
+    generateBtn.addEventListener('click', generatePassword);
+    copyBtn.addEventListener('click', copyToClipboard);
+
+    function generatePassword() {
+        const length = 12;
+        const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+        let password = '';
+
+        for (let i=0; i<length; i++){
+            const randomindex = Math.floor(Math.random() * chars.length);
+            password += chars[randomindex];
+        }
+
+        passwordField.value = password;
+        animateButton(generateBtn);
+    }
+
+    function copyToClipboard() {
+        if (passwordField.value) {
+            passwordField.select();
+            document.execCommand('copy');
+
+            copyBtn.innerHTML = "<i class='fas fa-check'></i>";
+
+            setTimeout(() => {
+                copyBtn.innerHTML = "<i class='fas fa-copy'></i>";
+            }, 1500);
+        }
+    }
+
+    function animateButton(button){
+        button.style.transform = 'scale(0.9)';
+
+        setTimeout(() => {
+            button.style.transform = 'scale(1)';
+        }, 150);
+    }
+
+    generatePassword
+});
